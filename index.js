404b087ff0420407078412e68d487b99f3bacf77
Reginaldo Santos <rmsantos@br.ibm.com>
2019-04-12 17:21:03 -0300

    initial tests with the sqlite database

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+const sqlite = require("sqlite");
+const dbConnection = sqlite.open("jobify.sqlite", { Promise });
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get("/", async (req, res) => {
+  const db = await dbConnection;
+  const categoriasDb = await db.all("SELECT * from categorias;");
+  const vagas = await db.all("SELECT * from vagas;");
+
+  const categorias = categoriasDb.map(cat => {
+    return {
+      ...cat,
+      vagas: vagas.filter(vaga => vaga.categoria === cat.id)
+    };
+  });
+
+  res.render("home", {
+    categorias
+  });
 });
 
 app.get("/vaga", (req, res) => {
   res.render("vaga");
 });
+
+const init = async () => {
+  const db = await dbConnection;
+  await db.run(
+    "create table if not exists categorias (id INTEGER PRIMARY KEY, categoria TEXT);"
+  );
+  await db.run(
+    "create table if not exists vagas (id INTEGER PRIMARY KEY, categoria INTEGER, titulo TEXT, descricao TEXT);"
+  );
+  //const categoria = 'Marketing team';
+  // const cat = 2
+  // const vagaTitulo = 'Digital Marketing (San Francisco)'
+  // const vagaDescr = 'excelent benefits'
+  // await db.run(`INSERT INTO vagas(categoria, titulo, descricao) values(${cat}, '${vagaTitulo}', '${vagaDescr}')`)
+};
+
+init();
 
 app.listen(3000, err => {
   if (err) {
