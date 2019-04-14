dfde5c0b67bbb5617fb4f2d6bfe1e62326d55d67
Reginaldo Marcelo dos Santos <reginaldosantos.br@gmail.com>
2019-04-13 23:32:44 -0300

    publishing this project on zeit.co

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const sqlite = require("sqlite");
 const dbConnection = sqlite.open("jobify.sqlite", { Promise });
 
+const port = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -107,7 +109,7 @@ const init = async () => {
 
 init();
 
-app.listen(3000, err => {
+app.listen(port, err => {
   if (err) {
     console.log("não foi possível iniciar o servidor");
   } else {
