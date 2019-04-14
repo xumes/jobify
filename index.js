06a4d1cb447a32b084cb010f373b2c849c5ab117
Reginaldo Marcelo dos Santos <reginaldosantos.br@gmail.com>
2019-04-14 00:06:55 -0300

    fixing public path

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const dbConnection = sqlite.open(path.resolve(__dirname, "jobify.sqlite"), {
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
